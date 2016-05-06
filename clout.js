74eea9b216a79f5e2ddc92fd14cf361826080407
joshua-anderson <j@joshua-anderson.com>
2016-05-05 21:19:30 -0700

    Add and remove user from firebase

--- clout.js
+++ clout.js
@@ -1,12 +1,34 @@
 var vue;
+
 var clout = {
 	el: "body",
 	data: {
-		username: ""
+		username: "",
+		playing: false
 	},
 	methods: {
 		login: function() {
-			alert("Welcome, President " + this.username + "!");
+			if(this.playing) {
+				alert("You're already playing!");
+				return;
+			}
+
+			db = new Firebase("https://apesclout.firebaseio.com/users/")
+
+			db.once("value", function(ref) {
+
+				if(ref.child(clout.data.username + "/country").exists()) {
+					alert(clout.data.username + " is already in use");
+					return;
+				}
+
+				db.child(clout.data.username + "/country").set(1);
+				alert("Welcome, President " + clout.data.username + "!");
+
+				db.child(clout.data.username).onDisconnect().remove();
+
+				clout.data.playing = true;
+			});
 		}
 	}
 };
